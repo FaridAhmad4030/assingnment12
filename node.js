b1aba871a09dea6adaabe28c6f77379b38cfd6f2
farid <frd.mirzadah@gmail.com>
2024-09-12 23:09:51 -0700

    Example-4

--- node.js
+++ node.js
@@ -15,3 +15,10 @@ console.log( `Breed: ${dog.breed}`);
 //Example-3
 const fruits = ['Apple', 'Banana','cherry'];
 console.log(fruits[0]);
+
+//Example-4
+function doubleNumber(x){
+    return x * 2;
+}
+const result = doubleNumber(5);
+console.log(result);

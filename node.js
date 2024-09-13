b96b4629ee840b213d91d4949ba2b7d8b732406b
farid <frd.mirzadah@gmail.com>
2024-09-12 23:00:32 -0700

    Example-2

--- node.js
+++ node.js
@@ -3,3 +3,11 @@ function greet(name) {
     return `Hello, ${name}!`;
 }
 console.log (greet("Alice"));
+
+//Example-2
+const dog = {
+    name: 'Buddy',
+    breed: 'Golden Retriever'
+};
+console.log( `Name: ${dog.name}`);
+console.log( `Breed: ${dog.breed}`);
